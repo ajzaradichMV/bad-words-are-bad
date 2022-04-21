aa6d6c4f2121e8304810e22a78ab2533f0a3cb8b
ajzaradichMV <anthony@mediavine.com>
2022-04-21 18:51:08 -0500

    Added tons of stuff
    - Added first API endpoint to allow myself to add in sites by hitting the endpoint. This SHOULD ideally be removed in a later update, right now this is just for ease.
    - Reworked some file locations, still more work to be done there.
    - Ensured scraper functionality. The tool will now search through sitemap xmls and load in those additional xmls and try to find the posts within.

--- main.mjs
+++ main.mjs
@@ -1,5 +1,6 @@
 import express from 'express'
 import xmlScrape from './lib/js/scraper.js'
+import {databaseRun} from './lib/js/database.mjs'
 
 const app = new express()
 
@@ -15,4 +16,36 @@ app.get('/', (req, res) => {
     xmlScrape('https://support-sandbox.com/wp-sitemap-posts-post-1.xml').then( (ret)=> {
         res.json(ret)
     })
+})
+
+/**
+ * Endpoint: /api/v1
+ * 
+ * Query params: 
+ * 
+ * site - Takes an URL location as a parameter and passes it to the API. Requires a sitemap.xml file.
+ * force - Accepts either "true" or "false" and if set to "true" will force the record to be updated even if less than 30 days. Defaults to false.
+ * 
+ */
+app.get('/api/v1', (req, res) => {
+    let siteXML = '';
+    let force = 'false';
+
+    if (req.query.site == undefined) {
+        res.json ('Requires site xml. Use query param: ?site=<link here>');
+    } else {
+        siteXML = req.query.site;
+    }
+
+    if (req.query.force == undefined) {
+        req.query.force = 'false';
+    } else {
+        if ( !(req.query.force == 'false' || req.query.force == 'true') ) {
+            res.json('Force must be true or false. Default: false');
+        }
+    }
+    force = req.query.force;
+
+    databaseRun(siteXML, force)
+    res.json(siteXML)
 })
